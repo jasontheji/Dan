24e56616040529dae395163fb5b7a96a77b4ff74
jasontheji <jasonjicarter@gmail.com>
2018-05-24 11:07:00 -0700

    Add another comments

--- Dan.py
+++ Dan.py
@@ -2,6 +2,8 @@
 from tensorflow.examples.tutorials.mnist import input_data
 
 import tensorflow as tf
+
+# assign variable
 sess = tf.InteractiveSession()
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
